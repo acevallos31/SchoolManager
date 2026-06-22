90a62003c500fadaa7f9cb03414b4a99e3fbe8ff
acevallos31 <67563695+acevallos31@users.noreply.github.com>
2026-06-22 00:03:51 -0600

    Update jwt.interceptor.ts

--- frontend/schoolmanager-frontend/src/app/core/interceptors/jwt.interceptor.ts
+++ frontend/schoolmanager-frontend/src/app/core/interceptors/jwt.interceptor.ts
@@ -8,17 +8,23 @@ import {
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
-// Inyecta automáticamente el token JWT en cada petición saliente hacia la API
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+
   constructor(private authService: AuthService) {}
 
-  intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(
+    req: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+
     const token = this.authService.getToken();
 
     if (token) {
       req = req.clone({
-        setHeaders: { Authorization: `Bearer ${token}` }
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
       });
     }
 
